ea45742d473b8aba013ab5060e1bd81dbb43d1a1
Darrel Sharpe <dsharpe@dsharpe-mbp.home>
2015-02-25 14:37:07 -0600

    change version number

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings St Louis Roadshow!";
+        return "Greetings Dallas Roadshow!";
     }
 }
